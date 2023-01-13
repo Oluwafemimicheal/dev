2444d2efd54decd5a429f96eb73b6dae877b004f
oluwafemimicheal <oluwafemicu@gmail.com>
2023-01-13 16:36:49 +0100

    master

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,9 @@
-console.log('Working')
+const header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
