8f1d408462d709173377a8e61cf5f86de195bf91
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-18 23:15:53 +0100

    initial commit

--- MailTunnels/main.js
+++ MailTunnels/main.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector('.fa-bars')
 const navbarToggle = document.querySelector('.navbar')
-const header
+const header = document.querySelector('.header')
 
 menu.addEventListener('click', () => {
     navbarToggle.classList.toggle('active')
-
 })
+
+header.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
+console.log(window.scrollY)
