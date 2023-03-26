4ea3d5b46e198195c97078b10efa60b5e8216c0a
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-26 15:10:54 +0100

    initial commit

---
+++ MailTunnels/form.js
@@ -0,0 +1,54 @@
+const register = document.querySelector('.register')
+const user = document.querySelector('.user')
+const password = document.querySelectorAll('.show-password')
+const icon = document.querySelectorAll('.icon img')
+const form = document.querySelectorAll('form')
+const input = document.querySelectorAll('form input')
+const message = document.querySelector('.message-popup')
+const submit = document.querySelector('#submit')
+
+
+const Register = () => {
+    register.style.display = 'flex'
+    user.style.display = 'none'
+}
+const User = () => {
+    user.style.display = 'flex'
+    register.style.display = 'none'
+}
+
+let valueOfInput = input.value
+if (valueOfInput.length < 8) {
+    message
+}
+
+icon.forEach((icon) => {
+    icon.addEventListener('click', (e) => {
+        password.forEach((password) => {
+            icon.src = 'image/eye-slash-svgrepo-com.svg'
+            password.type = 'text'
+            setTimeout(() => {
+                password.type = 'password'
+                icon.src = 'image/eye-svgrepo-com.svg'
+            }, 1500)
+        })
+    })
+})
+
+form.forEach((form) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        submit.addEventListener('click', () => {
+
+        })
+    })
+})
+
+
+let name = "jinadu oluwafemi"
+
+if (name.length < 8) {
+    console.log('try again')
+} else {
+    console.log('it work')
+}
