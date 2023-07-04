b97cff56ac69a817c237a5764d72809077d0efae
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-04 20:07:13 +0100

    initial commit

--- app.js
+++ app.js
@@ -5,24 +5,26 @@ const { logger, authorize } = require('./middleware')
 
 // req=> Middleware =>res
 
-app.use(logger) // can also apply a path which make all that path route to access the logger functionality example: /api/product
-app.use(express.static('./public'))
+//app.use(logger) // can also apply a path which make all that path route to access the logger functionality example: /api/product
 
 app.get('/', (req, res) => {
-    res.send('<h1>Middleware Page</h1><a href="/logger">Check Middleware</a>')
-})
-app.get('/shop', (req, res) => {
-    res.sendFile(path.resolve('./public/shop.html'))
+        res.send('<h1>Middleware Page</h1><a href="/logger">Check Middleware</a>')
+    })
+    // app.use(express.static('./form'))
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/form', (req, res) => {
+    res.sendFile(path.resolve('./form/contact.html'))
 })
 
-app.get('/api/products', authorize, (req, res) => {
-    console.log(req.user)
-    res.send('<h1>Products</h1><a href="/logger">Check Middleware</a>')
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    console.log(name)
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide Credentials')
 })
-app.get('/api/item', authorize, (req, res) => {
-    res.send('<h1>Items</h1><a href="/logger">Products</a>')
-})
-
 
 app.get('*', (req, res) => res.status(404).send('<h1>Page not found</h1><a href="/">Item</a>'))
 
